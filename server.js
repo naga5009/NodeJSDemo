9f5226b5ad87c0010b66bbf1c9276935db142dbd
naga5009 <92075813+naga5009@users.noreply.github.com>
2021-11-14 14:26:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome from a Node.js app Nov13th!')
+  res.send('Welcome from a Node.js app Nov14th!')
 })
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
